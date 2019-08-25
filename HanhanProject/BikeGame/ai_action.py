75fc363395b9221e20ba7b88284c96b5411c38e1
71Y17109 <642214925@qq.com>
2019-08-25 09:54:21 +0800

    赵子轩 2019/8/25 2019/8/25 9.55

---
+++ HanhanProject/BikeGame/ai_action.py
@@ -0,0 +1,13 @@
+from Interaction.my_keyboard import *
+#周智圆 2019.8.23
+#AI操作函数
+#action[left_arrow,right_arrow,spacebar]:1表示执行，0表示不执行
+def game_ai_action(action):
+    if action[0]==1:
+        key_tap('left_arrow')
+    elif action[1]==1:
+        key_tap('right_arrow')
+    elif action[2]==1:
+        key_tap('spacebar')
+    else:
+        pass
