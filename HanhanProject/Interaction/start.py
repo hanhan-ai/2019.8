0bcf86304a1aa81e6342b4f5d402f624c279ee8c
ZhiyuanXinfang <1379612504@qq.com>
2019-08-25 22:38:26 +0800

    周智圆 2019.8.23 出错的

--- HanhanProject/Interaction/start.py
+++ HanhanProject/Interaction/start.py
@@ -23,9 +23,15 @@ from BikeGame.reward_handle import *
 
 from my_keyboard import *
 
+from DeepQNetworkBall.Network import *
+
 PATH='H:\kk'#截图储存位置
 SCREEN_SHOT_TIME=0.04#截屏间隔时间
 LAST_ADR = 0 #上一张图肾上腺素值
+left=0
+right=0
+top=0
+bottom=0
 
 #周智圆 2019.8.23
 #程序界面函数
@@ -33,23 +39,13 @@ def first_window(top):
     stop_button = Button(top, text="点我终止程序", command=sys.exit)
     stop_button.pack()
 
-#周智圆 2019.8.25
-#通用转化函数函数
-#acton:游戏操作
-#rw:reward  frame:环境状态数组
-def game_convertion(action):
-    global left, top, right, bottom#截图窗口位置
-    game_ai_action(action)
-    img = pyautogui.screenshot(region=[left, top, right-left, bottom-top])
-    rw=reward_handle(img,LAST_ADR)
-    frame=pic_change(img)
-    return rw,frame
 
 #周智圆 2019.8.23
 # 鼠标左击事件处理函数
 clicktime=[0]#记录每次点击事件时间
 def StartMouseEvent(event):
     global handle,clickn,clicktime
+    global left, top, right, bottom#截图窗口位置
     clicktime.append(time.time())                #事件发生的时间
     handle=event.Window            #窗口句柄
     print(handle)
@@ -74,8 +70,25 @@ def StartMouseEvent(event):
         #模拟游戏输入
         game_base_action()
 
+        #开始进行神经网络的循环
+        #赵士陆 2019.8.25  20：54
+        startNetwork()
+
         # 返回True代表将事件继续传给其他句柄，为False则停止传递，即被拦截
     return True
+
+#周智圆 2019.8.25
+#通用转化函数函数
+#acton:游戏操作
+#rw:reward  frame:环境状态数组
+def game_convertion(action):
+    global left, top, right, bottom  # 截图窗口位置
+    game_ai_action(action)
+    img = pyautogui.screenshot(region=[left, top, right-left, bottom-top])
+    rw=reward_handle(img,LAST_ADR)
+    frame=pic_change(img)
+    return rw,frame
+
 
 #周智圆 2019.8.23
 #监听鼠标左击事件函数
