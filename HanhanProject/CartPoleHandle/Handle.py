81fa0220c5f4e5ffcaf8f2afb71b958341762862
LobotomyToCalm <lynlonelygod@163.com>
2019-08-30 16:12:55 +0800

    2019.8.30 16:12 赵士陆 创建第二个游戏的handle

---
+++ HanhanProject/CartPoleHandle/Handle.py
@@ -0,0 +1,17 @@
+import gym
+import DeepQNetwork.Network as DQN
+
+
+env = gym.make('CartPole-v0')
+
+
+
+def PictureHandle():
+    pass
+
+def ActionHandle():
+    pass
+
+def RewardHandle():
+    pass
+
