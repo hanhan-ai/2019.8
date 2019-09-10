876388bf1c40483c298cd33a63226a01ef1fd16f
LobotomyToCalm <lynlonelygod@163.com>
2019-09-10 10:58:18 +0800

    zsl 2019.9.10 10:58 添加了注释

--- HanhanProject/GApart/ExampleGame.py
+++ HanhanProject/GApart/ExampleGame.py
@@ -19,7 +19,7 @@ HIDDENLAYER_2 = 5
 ACTION = 2
 
 env = gym.make('CartPole-v0')
-population = Population(3)
+population = Population(10)
 population.initPopulation(OBSERVE_NUM, HIDDENLAYER_1, HIDDENLAYER_2, ACTION)
 
 population.runGame(env)
--- HanhanProject/HanhanAI_0/CartPoleHandle/handle.py
+++ HanhanProject/HanhanAI_0/CartPoleHandle/handle.py
@@ -1,3 +1,10 @@
+"""
+作者：赵士陆
+创建时间：2019.8.24
+最后一次修改时间：2019.8.30
+
+"""
+
 import gym
 import numpy as np
 import cv2
@@ -6,24 +13,24 @@ import cv2
 env = gym.make('CartPole-v0')
 
 action_number = env.action_space
-stay_action = 1     # 其实不是
-def CartPolePictureHandle(observation):
+stay_action = 1     # 暂定为1
+def CartPolePictureHandle(observation):     # Process the picture of the game as the input of the network
     cp_observation_origin = cv2.cvtColor(observation, cv2.COLOR_BGR2GRAY)
     # cp_observation = cv2.resize(cp_observation_origin, (inputImageSize[1], inputImageSize[0]))
     print("def CartPolePictureHandle(observation)","cp_observation_origin:  ",cp_observation_origin)
     return cp_observation_origin
 
 
-def CartPoleActionHandle(act):
+def CartPoleActionHandle(act):              # Process the action of the game as the output of the network
     for k in range(0, len(act)):
         if act[k] == 1:
             act_return = k
         else:
             pass
-    print("def CartPoleActionHandle(act)","act_return:  ",act_return)
+    print("def CartPoleActionHandle(act)", "act_return:  ", act_return)
     return act_return
 
-def Game():
+def Game():                                 # Game cycle
 
         ob = env.reset()
 
